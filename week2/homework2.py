d3cd56c890cfc9d91c2aa0f74eccb4a51830e700
Liana <lianavarosyan@gmail.com>
2020-04-29 14:49:33 +0400

    updated 6

--- week2/homework2.py
+++ week2/homework2.py
@@ -92,10 +92,56 @@ def heapq(a,b):
 heapq([1],2)
 
 
-# In[ ]:
+# In[1]:
 
 
+# 6
+def merge_sort(main_list,*args):
+    lenght = len(main_list)
+    if lenght > 1:
+        mid = lenght // 2
+        left_list = main_list[:mid]
+        right_list = main_list[mid:]
+        
+        merge_sort(left_list, *args) #recursion for sublists
+        merge_sort(right_list, *args)
+        
+        i = j = k = 0 #merging
+        while i < len(left_list) and j < len(right_list):
+            
+            if len(args) > 0 and args [0] == "descending":
+                if left_list[i] >= right_list[j]:
+                    main_list[k] = left_list[i]
+                    i += 1
+                    k += 1
+                else:
+                    main_list[k] = right_list[j]
+                    j += 1
+                    k += 1
+            else:
+                if left_list[i] >= right_list[j]:
+                    main_list[k] = right_list[j]
+                    j += 1
+                    k += 1
+                else:
+                    main_list[k] = left_list[i]
+                    i += 1
+                    k += 1
+                  
+            while i < len(left_list): #if elements of one list end, but remain elements of second list
+                main_list[k] = left_list[i]
+                i += 1
+                k += 1
 
+            while j < len(right_list):
+                main_list[k] = right_list[j]
+                j += 1
+                k += 1
+           
+        return main_list
+        
+    
+merge_sort([1,4,7,7,8,0,100],"descending")
 
 
 # In[ ]:
